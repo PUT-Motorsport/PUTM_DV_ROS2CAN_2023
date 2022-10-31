7b840dface7f6c9744ae5d957c3a4091cc0103f6
KozAAAAA <mateusz-koza@outlook.com>
2022-10-31 19:41:43 +0100

    main.cpp every function explained

--- main.cpp
+++ main.cpp
@@ -17,9 +17,9 @@ int main(void)
 	
     int nbytes;                         // number of bytes wrote by function wirte()
 
-	struct sockaddr_can addr;           // structure that holds information about a socket
+	struct sockaddr_can addr;           // structure that holds information relevant for bind()
 	struct can_frame frame;             // can frame that is later sent
-	struct ifreq ifr;                   // structure that is returned by ioctl()
+	struct ifreq ifr;                   // structure that is used to get network index based on the name
 
 	const char *ifname = "slcan0";      // network interface name
 
@@ -30,20 +30,25 @@ int main(void)
 		perror("Error while opening socket");
 		return -1;
 	}
-    
+
 	strcpy(ifr.ifr_name, ifname);       // assign interface name to ifr structure
 
+    /*
+    The socket descriptor is merely an artefact of the way in which ioctl()
+    commands are invoked generally, and is not used for any particular purpose
+    by SIOCGIFINDEX. It must be open and must refer to a socket.
+    */
 	ioctl(s, SIOCGIFINDEX, &ifr);       // return network index number via ifr structure
 	
 	addr.can_family  = AF_CAN;          // set the address family type
 	addr.can_ifindex = ifr.ifr_ifindex; // pass the network interface index
 
-	printf("%s at index %d\n", ifname, ifr.ifr_ifindex);
-
-	if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
+	if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) == -1)  // assign network interface to a socket
+    {
 		perror("Error in socket bind");
 		return -2;
 	}
+
 
 	frame.can_id  = 0x123;
 	frame.can_dlc = 2;
