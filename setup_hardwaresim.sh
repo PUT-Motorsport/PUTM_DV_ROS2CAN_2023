e2ce57ee0d48f10b65384ba77f8c314355692ea4
KozAAAAA <mateusz-koza@outlook.com>
2022-11-30 12:22:16 +0100

    harware simulation with value changing

--- setup_hardwaresim.sh
+++ setup_hardwaresim.sh
@@ -9,23 +9,40 @@ ip link add dev slcan0 type vcan
 ip link set up slcan0
 
 
-uint32_t="00000A"
-int32_t="00000A"
-uint16_t="000A"
-int16_t="000A"
-uint8_t="0A"
-int8_t="0A"
-states="01"
-bool="01"
-
-#cokolwiek innego niż coś z powyższych to uint8
-
 # CANbus frames generation
+
+i=0
+
 while true
 do
-        cansend slcan0 "005#${uint8_t}${uint8_t}${states}"
-        cansend slcan0 "00A#${uint8_t}${uint16_t}"
-    
-    done
+    if [ $i -eq 1 ]
+    then
+        uint32_t="00000B"
+        int32_t="00000B"
+        uint16_t="000B"
+        int16_t="000B"
+        uint8_t="0B"
+        int8_t="0B"
+        states="00"
+        bool="00"
+
+        i=0
+    else
+        uint32_t="00000A"
+        int32_t="00000A"
+        uint16_t="000A"
+        int16_t="000A"
+        uint8_t="0A"
+        int8_t="0A"
+        states="01"
+        bool="01"
+
+        i=1
+    fi
+
+        cansend slcan0 "005#${uint8_t}"
+        cansend slcan0 "00A#${uint8_t}${uint8_t}${uint8_t}"
+        cansend slcan0 "00C#${uint8_t}${uint16_t}"
+
 done
 
